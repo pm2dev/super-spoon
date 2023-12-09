8f3ba1f7ac7f0713027be5a4c99999a91acafdd7
Rob Garcia <robgarcia284@gmail.com>
2023-12-08 21:13:54 -0600

    fixed javascript code to make the app work properly

--- script.js
+++ script.js
@@ -1,64 +1,97 @@
-
-
-// Selecting the input field with the class "task-input" and tag name "input"
 const taskInput = document.querySelector(".task-input input"),
-
-// Selecting all elements with the tag name "span" under elements with the class "filters"
 filters = document.querySelectorAll(".filters span"),
-
-// Selecting an element with the class "clear-btn"
 clearAll = document.querySelector(".clear-btn"),
-
-// Selecting an element with the class "task-box"
 taskBox = document.querySelector(".task-box");
-
-let editId,           // Declaration of a variable named 'editId' (possibly used for tracking the ID of the task being edited)
-    isEditTask = false,  // Declaration and initialization of a variable named 'isEditTask' with a value of false
-    todos = JSON.parse(localStorage.getItem("todo-list"));  // Declaration and initialization of a variable named 'todos' by retrieving and parsing data from local storage
-
-
-    function showTodo(filter) {
-        // Declare a variable to store HTML string
-        let liTag = "";
-    
-        // Check if 'todos' array is defined and not empty
-        if (todos) {
-            // Iterate over each 'todo' in 'todos' array along with its index 'id'
-            todos.forEach((todo, id) => {
-                // Determine if the task is completed and set the 'completed' variable accordingly
-                let completed = todo.status == "completed" ? "checked" : "";
-    
-                // Check if the task matches the specified filter or the filter is set to "all"
-                if (filter == todo.status || filter == "all") {
-                    // Construct the HTML string for the task and append it to 'liTag'
-                    liTag += `<li class="task">
-                                <label for="${id}">
-                                    <input onclick="updateStatus(this)" type="checkbox" id="${id}" ${completed}>
-                                    <p class="${completed}">${todo.name}</p>
-                                </label>
-                                <div class="settings">
-                                    <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
-                                    <ul class="task-menu">
-                                        <li onclick='editTask(${id}, "${todo.name}")'><i class="uil uil-pen"></i>Edit</li>
-                                        <li onclick='deleteTask(${id}, "${filter}")'><i class="uil uil-trash"></i>Delete</li>
-                                    </ul>
-                                </div>
-                            </li>;
-                }
-            });
-        }
-    
-        // Set the innerHTML of 'taskBox' with the constructed HTML string or a message if no tasks
-        taskBox.innerHTML = liTag || '<span>You don't have any task here</span>';
-
-
-        // Select all elements with class "task" inside 'taskBox'
-        let checkTask = taskBox.querySelectorAll(".task");
-
-        // If there are no tasks, remove the "active" class from 'clearAll'; otherwise, add the "active" class
-        !checkTask.length ? clearAll.classList.remove("active") : clearAll.classList.add("active");
-
-        // If the height of 'taskBox' is greater than or equal to 300, add the "overflow" class; otherwise, remove it
-        taskBox.offsetHeight >= 300 ? taskBox.classList.add("overflow") : taskBox.classList.remove("overflow");
-        taskBox.offsetHeight >= 300 ? taskBox.classList.add("overflow") : taskBox.classList.remove("overflow");
+let editId,
+isEditTask = false,
+todos = JSON.parse(localStorage.getItem("todo-list"));
+filters.forEach(btn => {
+    btn.addEventListener("click", () => {
+        document.querySelector("span.active").classList.remove("active");
+        btn.classList.add("active");
+        showTodo(btn.id);
+    });
+});
+function showTodo(filter) {
+    let liTag = "";
+    if(todos) {
+        todos.forEach((todo, id) => {
+            let completed = todo.status == "completed" ? "checked" : "";
+            if(filter == todo.status || filter == "all") {
+                liTag += `<li class="task">
+                            <label for="${id}">
+                                <input onclick="updateStatus(this)" type="checkbox" id="${id}" ${completed}>
+                                <p class="${completed}">${todo.name}</p>
+                            </label>
+                            <div class="settings">
+                                <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
+                                <ul class="task-menu">
+                                    <li onclick='editTask(${id}, "${todo.name}")'><i class="uil uil-pen"></i>Edit</li>
+                                    <li onclick='deleteTask(${id}, "${filter}")'><i class="uil uil-trash"></i>Delete</li>
+                                </ul>
+                            </div>
+                        </li>`;
+            }
+        });
     }
+    taskBox.innerHTML = liTag || `<span>You don't have any task here</span>`;
+    let checkTask = taskBox.querySelectorAll(".task");
+    !checkTask.length ? clearAll.classList.remove("active") : clearAll.classList.add("active");
+    taskBox.offsetHeight >= 300 ? taskBox.classList.add("overflow") : taskBox.classList.remove("overflow");
+}
+showTodo("all");
+function showMenu(selectedTask) {
+    let menuDiv = selectedTask.parentElement.lastElementChild;
+    menuDiv.classList.add("show");
+    document.addEventListener("click", e => {
+        if(e.target.tagName != "I" || e.target != selectedTask) {
+            menuDiv.classList.remove("show");
+        }
+    });
+}
+function updateStatus(selectedTask) {
+    let taskName = selectedTask.parentElement.lastElementChild;
+    if(selectedTask.checked) {
+        taskName.classList.add("checked");
+        todos[selectedTask.id].status = "completed";
+    } else {
+        taskName.classList.remove("checked");
+        todos[selectedTask.id].status = "pending";
+    }
+    localStorage.setItem("todo-list", JSON.stringify(todos))
+}
+function editTask(taskId, textName) {
+    editId = taskId;
+    isEditTask = true;
+    taskInput.value = textName;
+    taskInput.focus();
+    taskInput.classList.add("active");
+}
+function deleteTask(deleteId, filter) {
+    isEditTask = false;
+    todos.splice(deleteId, 1);
+    localStorage.setItem("todo-list", JSON.stringify(todos));
+    showTodo(filter);
+}
+clearAll.addEventListener("click", () => {
+    isEditTask = false;
+    todos.splice(0, todos.length);
+    localStorage.setItem("todo-list", JSON.stringify(todos));
+    showTodo()
+});
+taskInput.addEventListener("keyup", e => {
+    let userTask = taskInput.value.trim();
+    if(e.key == "Enter" && userTask) {
+        if(!isEditTask) {
+            todos = !todos ? [] : todos;
+            let taskInfo = {name: userTask, status: "pending"};
+            todos.push(taskInfo);
+        } else {
+            isEditTask = false;
+            todos[editId].name = userTask;
+        }
+        taskInput.value = "";
+        localStorage.setItem("todo-list", JSON.stringify(todos));
+        showTodo(document.querySelector("span.active").id);
+    }
+});
